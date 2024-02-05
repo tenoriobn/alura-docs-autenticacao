bbb41ca70f8942e18710d64db44339ce128ce855
Bruno Tenório <tenoriobn@gmail.com>
2024-02-05 16:19:34 -0300

    Cadastrando no banco de dados

--- public/cadastro/socket-front-cadastro.js
+++ public/cadastro/socket-front-cadastro.js
@@ -4,4 +4,8 @@ function emitirCadastrarUsuario(dados) {
   socket.emit("cadastrar_usuario", dados);
 }
 
+socket.on("cadastro_sucesso", () => alert("Cadastro realizado com sucesso!"));
+socket.on("cadastro_erro", () => alert("Erro no cadastro!"));
+socket.on("usuario_ja_existente", () => alert("Usuário já existe!"));
+
 export { emitirCadastrarUsuario };
--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -2,17 +2,18 @@ import { MongoClient } from "mongodb";
 
 const cliente = new MongoClient("mongodb+srv://alura:123@cluster0.jlnbz7x.mongodb.net/?retryWrites=true&w=majority");
 
-let documentosColecao;
+let documentosColecao, usuariosColecao;
 
 try {
   await cliente.connect();
 
   const db = cliente.db("alura-websockets");
   documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios")
 
   console.log("Conectado ao banco de dados com sucesso!");
 } catch (erro) {
   console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
