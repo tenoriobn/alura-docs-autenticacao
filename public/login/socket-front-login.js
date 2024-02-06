4a8c3fedfd8bf07520662abf8a1cbbc6e9c84512
Bruno Tenório <tenoriobn@gmail.com>
2024-02-06 15:09:23 -0300

    Autenticando usuários

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -4,4 +4,12 @@ function emitirAutenticarUsuario(dados) {
   socket.emit("autenticar_usuario", dados);
 }
 
+socket.on("autenticacao_sucesso", () => {
+  alert("Usuário autenticado com sucesso!");
+
+  window.location.href = "/";
+});
+socket.on("autenticacao_erro", () => alert("Erro na autenticação!"));
+socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado!"));
+
 export { emitirAutenticarUsuario }
