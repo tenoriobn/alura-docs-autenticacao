d6009c0ef92c373c74b185d8df67f14b1f2a506f
Bruno Tenório <tenoriobn@gmail.com>
2024-02-07 15:19:20 -0300

    Guardando JWT nos cookies

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,12 +1,15 @@
+import { definirCookie } from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados) {
   socket.emit("autenticar_usuario", dados);
 }
 
-socket.on("autenticacao_sucesso", () => {
-  alert("Usuário autenticado com sucesso!");
+socket.on("autenticacao_sucesso", (tokenJwt) => {
+  definirCookie("tokenJwt", tokenJwt);
 
+  alert("Usuário autenticado com sucesso!");
   window.location.href = "/";
 });
 socket.on("autenticacao_erro", () => alert("Erro na autenticação!"));
