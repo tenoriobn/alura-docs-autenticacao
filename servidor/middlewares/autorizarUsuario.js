0af3b9505aa8646d56d35bbf226c745e84591d73
Bruno Tenório <tenoriobn@gmail.com>
2024-02-07 16:17:53 -0300

    autorizando usuários

--- servidor/middlewares/autorizarUsuario.js
+++ servidor/middlewares/autorizarUsuario.js
@@ -1,5 +1,14 @@
+import jwt from "jsonwebtoken";
+
 function autorizarUsuario(socket, next) {
-  next();
+  const tokenJwt = socket.handshake.auth.token;
+
+  try {
+    jwt.verify(tokenJwt, process.env.SEGREDO_JWT);
+    next();
+  } catch (erro) {
+    next(erro);
+  }
 };
 
 export default autorizarUsuario;
